c0b65e92d7b30226a8d727345a5081209b3c5e64
scottyplunkett <splunkett@8thLight.com>
2018-02-09 17:18:16 -0600

    Serving files, WIP

--- src/com/scottyplunkett/server/com/scottyplunkett/server/Router.java
+++ src/com/scottyplunkett/server/com/scottyplunkett/server/Router.java
@@ -1,24 +1,26 @@
 package com.scottyplunkett.server;
 
-import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Router {
         Router(){}
 
-        public String route(String requestLine) throws MalformedURLException {
-            if (requestLine == "GET / HTTP/1.1") {
-                return "./helloworld.html";
+        public Path route(ArrayList<String> request) {
+            if ("GET / HTTP/1.1".equals(request.get(0))) {
+                return Paths.get("/Users/scottplunkett/Desktop/server/src/com/scottyplunkett/server/com/scottyplunkett/server/helloworld.html");
             }
-            else if (requestLine == "GET /nicole HTTP/1.1") {
-                return "com/scottyplunkett/server/nicole.html";
+            else if ("GET /nicole HTTP/1.1".equals(request.get(0))) {
+                return Paths.get("/Users/scottplunkett/Desktop/server/src/com/scottyplunkett/server/com/scottyplunkett/server/nicole.html");
             }
-            else if (requestLine == "GET /paul HTTP/1.1"){
-                return "./paul.html";
+            else if ("GET /paul HTTP/1.1".equals(request.get(0))){
+                return Paths.get("/Users/scottplunkett/Desktop/server/src/com/scottyplunkett/server/com/scottyplunkett/server/paul.html");
             }
-            else if (requestLine == "GET /josh HTTP/1.1") {
-                return "./josh.html";
+            else if ("GET /josh HTTP/1.1".equals(request.get(0))) {
+                return Paths.get("/Users/scottplunkett/Desktop/server/src/com/scottyplunkett/server/com/scottyplunkett/server/josh.html");
             } else {
-                return "com/scottyplunkett/server/404.html";
+                return Paths.get("/Users/scottplunkett/Desktop/server/src/com/scottyplunkett/server/com/scottyplunkett/server/404.html");
             }
         }
     }
