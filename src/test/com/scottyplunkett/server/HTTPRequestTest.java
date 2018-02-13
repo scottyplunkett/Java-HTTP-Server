81e92cd694bc133f947457ed923d56755f2fbbf6
scottyplunkett <splunkett@8thLight.com>
2018-02-13 00:13:18 -0600

    HTTP implementations of server, response, request, and router, w/ pages for resources

--- src/test/com/scottyplunkett/server/HTTPRequestTest.java
+++ src/test/com/scottyplunkett/server/HTTPRequestTest.java
@@ -1,8 +1,28 @@
 package com.scottyplunkett.server;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class HTTPRequestTest {
 
-//    void test() {
-
+    @Test
+    void testReadReturnsInputAsArray() throws IOException {
+        InputStream stream = new ByteArrayInputStream("requestLine\r\nline2\r\nline3\r\nline4".getBytes());
+        InputStreamReader feed = new InputStreamReader(stream);
+        BufferedReader in = new BufferedReader(feed);
+        ArrayList<String> requestList = new ArrayList<>();
+        Collections.addAll(requestList, new String[]{"requestLine", "line2", "line3", "line4"});
+        assertEquals(requestList, HTTPRequest.read(in));
+    }
+//
+//    @Test
+//    void testGetRequestLine() {
+//        HTTPRequest httpRequest = new HTTPRequest();
 //    }
+
 }
