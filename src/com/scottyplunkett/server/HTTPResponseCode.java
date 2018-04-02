1cb3d6478436a366295d0c6db0b89aa09130e6a9
scottyplunkett <splunkett@8thLight.com>
2018-04-02 18:29:33 -0500

    Adds logic for method not allowed spec.

--- src/com/scottyplunkett/server/HTTPResponseCode.java
+++ src/com/scottyplunkett/server/HTTPResponseCode.java
@@ -35,18 +35,14 @@ enum HTTPResponseCode {
         return status;
     }
 
-    public static int encode(String requestLine) {
-        String route = Parser.findRequestedRoute(requestLine);
-        String method = Parser.findRequestMethod(requestLine);
-        String methodsAllowed = AllowableRoutes.getAllowed(route);
+    public static int encode(String route) {
         switch (route) {
             case "/redirect": return 302;
             case "/foobar": return 404;
             case "/coffee": return 418;
-            default: return methodsAllowed.contains(method) ? 200 : 405;
+            default: return 200;
         }
     }
-
 
     private enum AllowableRoutes {
         FILE1("/file1", "GET"),
