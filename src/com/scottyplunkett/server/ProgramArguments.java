8b23358d558a18bf04d5c3524bbd5a50ab92e39c
scottyplunkett <splunkett@8thLight.com>
2018-04-12 21:11:34 -0500

    Added ProgramArgument class and test.

--- src/com/scottyplunkett/server/ProgramArguments.java
+++ src/com/scottyplunkett/server/ProgramArguments.java
@@ -3,53 +3,21 @@ package com.scottyplunkett.server;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 class ProgramArguments {
     private final int firstArg = 0;
     private String[] args;
+    private Stream<String> streamArgs;
     private int port;
     private Path root;
 
     ProgramArguments(String[] _args) {
         args = _args;
+        streamArgs = Arrays.stream(args);
         setPort();
         setRoot();
-    }
-
-
-
-    void setPort() {
-        for (int thisFlag = firstArg; thisFlag < args.length; thisFlag++ ) {
-            int flagValue = thisFlag + 1;
-            if(isPortFlag(args[thisFlag]) && isNumericalPortValue(args[flagValue])) {
-                port = Integer.parseInt(args[flagValue]);
-            }
-        }
-    }
-
-    void setRoot() {
-        for (int thisFlag = firstArg; thisFlag < args.length; thisFlag++ ) {
-            int flagValue = thisFlag + 1;
-            if(isDirectoryFlag(args[thisFlag]) && isExistingDirectory(args[flagValue])) {
-                root = Paths.get(args[flagValue]);
-            }
-        }
-    }
-
-    private boolean isPortFlag(String arg) {
-        return arg.equals("-p");
-    }
-
-    private static boolean isNumericalPortValue(String arg) {
-        return arg.matches("\\d+");
-    }
-
-    private boolean isExistingDirectory(String arg) {
-        return new File(arg).isDirectory();
-    }
-
-    private boolean isDirectoryFlag(String arg) {
-        return arg.equals("-d");
     }
 
     int getPort() {
@@ -58,5 +26,38 @@ class ProgramArguments {
 
     Path getRoot() {
         return root;
+    }
+
+    private boolean isPortFlag(String flag) {
+        return flag.equals("-p");
+    }
+
+    private static boolean isNumericalPortValue(String portArg) {
+        return portArg.matches("\\d+");
+    }
+
+    private boolean isExistingDirectory(String directoryArg) {
+        return new File(directoryArg).isDirectory();
+    }
+
+    private boolean isDirectoryFlag(String arg) {
+        return arg.equals("-d");
+    }
+
+    private void setPort() {
+        port = 5000;
+        for (int thisFlag = firstArg; thisFlag < args.length; thisFlag++) {
+            int flagValue = thisFlag + 1;
+            if (isPortFlag(args[thisFlag]) && isNumericalPortValue(args[flagValue]))
+                port = Integer.parseInt(args[flagValue]);
+        }
+    }
+
+    private void setRoot() {
+        for (int thisFlag = firstArg; thisFlag < args.length; thisFlag++) {
+            int flagValue = thisFlag + 1;
+            if (isDirectoryFlag(args[thisFlag]) && isExistingDirectory(args[flagValue]))
+                root = Paths.get(args[flagValue]);
+        }
     }
 }
